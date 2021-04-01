5cc8e6a1395fc7baec9ce4d9b9b8cf256ed9ecef
Melcolm Lee <melcolmlee@hotmail.com>
2021-04-02 02:16:15 +0800

    update functions

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,17 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions"),
-  PubSub = require(`@google-cloud/pubsub`),
+  PubSub = require('@google-cloud/pubsub'),
   admin = require("firebase-admin"),
-  BigQuery = require('@google-cloud/bigquery');
+  {BigQuery} = require('@google-cloud/bigquery');
+
+var unirest = require('unirest'); 
  
 const app = admin.initializeApp();
 const firestore = app.firestore();  
 const bigqueryClient = new BigQuery();
 
 
-exports.updateFireStore = functions.pubsub.topic('doorlockdata').onPublish((message) => {
+exports.updateReadingsFirestore = functions.pubsub.topic('truckupdate').onPublish((message) => {
 
     // Get attribute of the PubSub message JSON body.
     let devID, md, rd, temp, hum, ldr, ts = null;
@@ -32,30 +34,47 @@ exports.updateFireStore = functions.pubsub.topic('doorlockdata').onPublish((mess
     const collection = firestore.collection('truck_info');
     const updateDoc = collection.where ("devID", "==", devID).limit(1).get().then(query => {
         id = query.docs[0].id;
-        collection.doc(id).update({'temp' : temp});
+        collection.doc(id).update({'temp' : temp, 'md' : md, 'rd' : rd, 'hum' : hum, 'ldr' : ldr, 'ts' : ts});
         return console.log("Firestore update Success");
       });
 
+    });
+
+exports.updateReadingsBigQuery = functions.pubsub.topic('truckupdate').onPublish((message) => {
+
+      // Get attribute of the PubSub message JSON body.
+    let devID, md, rd, temp, hum, ldr, ts = null;
+    try {
+      devID = message.json.devID;
+      md = message.json.md;
+      rd = message.json.rd;
+      temp = message.json.temp;
+      hum = message.json.hum;
+      ldr = message.json.ldr;
+      ts = message.json.ts;
+      console.log(`Update from  ${devID} at ${ts}`);
+    } catch (e) {
+    console.error('PubSub message was not JSON', e);
+    }
+      
+    //Get current weather temp
+    //var req = unirest.get('https://api.data.gov.sg/v1/environment/air-temperature').end(function (res) { 
+    //    if (res.error) throw new Error(res.error);
+    //    console.log(res.raw_body);
+    //  });
+      
     //Update bigquery
-    //TODO
-    // Inserts the JSON objects into my_dataset:my_table.
-
-    /**
-     * TODO(developer): Uncomment the following lines before running the sample.
-     */
-    // const datasetId = 'my_dataset';
-    // const tableId = 'my_table';
+    const datasetId = 'truck_readings';
+    const tableId = 'readings';
     const rows = [
-        {name: 'Tom', age: 30},
-        {name: 'Jane', age: 32},
+        {devID: devID, temp: temp, hum: hum, md: md, rd: rd, ts: ts}
       ];
-  
-      // Insert data into a table
-      await bigquery
-        .dataset(datasetId)
-        .table(tableId)
-        .insert(rows);
-      console.log(`Inserted ${rows.length} rows`);
-    // [END bigquery_table_insert_rows]
-
+    
+    // Insert data into a table
+      try{
+      const updatebigquery= bigqueryClient.dataset(datasetId).table(tableId).insert(rows);
+      console.log(`Inserted ${rows}`);
+      }catch (e) {
+        console.error('Error inserting rows into bigquery', e);
+      }
     });
